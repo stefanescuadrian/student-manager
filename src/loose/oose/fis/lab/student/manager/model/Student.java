7ac8a555e4ae604f2006c241ecf57e6725948b1a
Borza Alexandru <alexborza860@yahoo.com>
2020-03-25 17:36:17 +0200

    SM-2 Implemented the Student model class

--- src/loose/oose/fis/lab/student/manager/model/Student.java
+++ src/loose/oose/fis/lab/student/manager/model/Student.java
@@ -1,8 +1,48 @@
 package loose.oose.fis.lab.student.manager.model;
 
+import java.util.Objects;
+
 public class Student {
     private String firstName;
     private String lastName;
     private int age;
     private double meanGrade;
+
+    public Student(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", meanGrade=" + meanGrade +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Double.compare(student.meanGrade, meanGrade) == 0 &&
+                firstName.equals(student.firstName) &&
+                lastName.equals(student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age, meanGrade);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
